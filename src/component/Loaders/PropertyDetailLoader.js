9badb8b22f9240e192b31d2928bf0da84f28475e
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2025-01-25 14:32:02 +0100

    Add files via upload

---
+++ src/component/Loaders/PropertyDetailLoader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropertyDetailLoader = () => {
+    return (
+        <div>
+            <div className='w-[100%] loader m-[auto] rounded-md h-[30rem] max-[617px]:h-[25rem] max-[480px]:h-[20rem] max-[390px]:h-[17rem] border-gray-300 border-[1px]'></div>
+        </div>
+    )
+}
+
+export default PropertyDetailLoader
